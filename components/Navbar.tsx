48424b1fa6d5b2094387d70993067b2b240e206d
vicktoh <ayomidevictor349@gmail.com>
2024-10-11 15:44:29 +0100

    feature: mobile responsivity

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,28 +1,48 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
 import { FaFire } from 'react-icons/fa'
+import { RiCloseFill } from 'react-icons/ri'
+import { RxHamburgerMenu } from 'react-icons/rx'
 
 const Navbar = () => {
     const router = useRouter()
     const activeColor = (p: string) => router.pathname === p
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
     return (
-        <div className='flex items-center justify-between text-white navbar py-3 px-40 sticky top-0'>
+        <div className='flex items-center justify-between text-white navbar py-3 px-5 md:px-8 lg:px-12 xl:px-28 2xl:px-36 3xl:px-40 sticky top-0'>
             <div className='flex items-center space-x-1'>
                 <FaFire color='#703BF7' size={32} />
                 <p>Estatein</p>
             </div>
-            <div className='flex items-center gap-x-10'>
+            <div className='hidden lg:flex items-center gap-x-10'>
                 <Link href="/" className={`px-7 py-4 border-2 border-transparent rounded-xl flex items-center justify-center ${activeColor('/') ? 'bg-linkBackground border-linkBorder' : 'hover:bg-linkBackground hover:border-linkBorder'}`}>
                     Home
                 </Link>
-                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-md flex items-center justify-center'>About Us</Link>
-                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-md flex items-center justify-center'>Properties</Link>
-                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-md flex items-center justify-center'>Services</Link>
+                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-md flex items-center justify-center'>
+                    About Us
+                </Link>
+                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-md flex items-center justify-center'>
+                    Properties
+                </Link>
+                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-md flex items-center justify-center'>
+                    Services
+                </Link>
             </div>
 
-            <div className='bg-buttonBackground py-4 px-6 flex items-center justify-center rounded-md border-linkBorder border'>
+            <div className='hidden lg:flex items-center justify-center bg-buttonBackground py-4 px-6 rounded-md border-linkBorder border'>
                 <Link href="#">Contact Us</Link>
+            </div>
+            <div className="relative w-6 h-6 overflow-hidden" onClick={toggleMenu}>
+                <div className={`absolute inset-0 transition-transform duration-300 ${isOpen ? 'rotate-180 scale-0' : 'rotate-0 scale-100'}`}>
+                    <RxHamburgerMenu className="w-full h-full" />
+                </div>
+                <div className={`absolute inset-0 transition-transform duration-300 ${isOpen ? 'rotate-0 scale-100' : '-rotate-180 scale-0'}`}>
+                    <RiCloseFill className="w-full h-full" />
+                </div>
             </div>
         </div>
     )
