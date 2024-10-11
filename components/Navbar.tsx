918231656f2278ac8010c15fd83b7904755d74e7
vicktoh <ayomidevictor349@gmail.com>
2024-10-11 14:07:45 +0100

    Navbar components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,30 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
+import { FaFire } from 'react-icons/fa'
 
 const Navbar = () => {
+    const router = useRouter()
+    const activeColor = (p: string) => router.pathname === p
     return (
-        <div>Navbar</div>
+        <div className='flex items-center justify-between text-white navbar py-3 px-40 sticky top-0'>
+            <div className='flex items-center space-x-1'>
+                <FaFire color='#703BF7' size={32} />
+                <p>Estatein</p>
+            </div>
+            <div className='flex items-center gap-x-10'>
+                <Link href="/" className={`px-7 py-4 border-2 border-transparent rounded-xl flex items-center justify-center ${activeColor('/') ? 'bg-linkBackground border-linkBorder' : 'hover:bg-linkBackground hover:border-linkBorder'}`}>
+                    Home
+                </Link>
+                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-md flex items-center justify-center'>About Us</Link>
+                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-md flex items-center justify-center'>Properties</Link>
+                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-md flex items-center justify-center'>Services</Link>
+            </div>
+
+            <div className='bg-buttonBackground py-4 px-6 flex items-center justify-center rounded-md border-linkBorder border'>
+                <Link href="#">Contact Us</Link>
+            </div>
+        </div>
     )
 }
 
