d584b805eb1900de4858defe0f85ff8a0fc5b72e
vicktoh <ayomidevictor349@gmail.com>
2024-10-13 13:44:17 +0100

    fix: Background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,10 @@ const config: Config = {
     extend: {
       backgroundColor: {
         "buttonBackground": "#141414",
-        "linkBackground": "var(--Grey-08, #141414);"
+        "linkBackground": "var(--Grey-08, #141414)",
+        "background": "var(--Grey-15, #262626)",
+        "secondaryButtonBackground": "var(--Purple-60, #703BF7)",
+        "gradientBackground": "linear-gradient(238deg, #2A213F 8.76%, rgba(25, 25, 25, 0.00) 50.09%), url('/images/herobannerimage.svg') lightgray 50% / cover no-repeat;"
       },
       borderColor: {
         "linkBorder": "var(--Grey-15, #262626)"
