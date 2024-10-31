4bbe58c6f009a92999a21f393cf936fe4c4696d1
vicktor349 <ayomidevictor349@gmail.com>
2024-10-31 16:45:46 +0100

    feature: Added mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { FaFire } from 'react-icons/fa'
 import { HiMiniBars3BottomRight } from 'react-icons/hi2'
 import { RiCloseFill } from 'react-icons/ri'
@@ -12,39 +12,74 @@ const Navbar = () => {
     const toggleMenu = () => {
         setIsOpen(!isOpen)
     }
+    const mobileRef = useRef<HTMLDivElement | null>(null)
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (mobileRef.current && !mobileRef.current.contains(e.target as Node)) {
+                setIsOpen(false);
+            }
+        }
+        if (isOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        // Clean up the event listener on component unmount
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isOpen])
     return (
-        <div className='flex items-center justify-between text-white navbar py-6 lg:py-3 px-5 md:px-8 lg:px-12 xl:px-28 2xl:px-36 3xl:px-40 sticky top-0 z-[9999]'>
-            <div className='flex items-center space-x-1'>
-                <FaFire color='#703BF7' size={32} />
-                <p>Estatein</p>
-            </div>
-            <div className='hidden lg:flex items-center gap-x-10'>
-                <Link href="/" className={`px-7 py-4 border rounded-xl flex items-center border-transparent justify-center ${activeColor('/') ? 'bg-linkBackground border-linkBorder border-2' : 'hover:bg-linkBackground hover:border-linkBorder'}`}>
-                    Home
-                </Link>
-                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-xl flex items-center justify-center'>
-                    About Us
-                </Link>
-                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-xl flex items-center justify-center'>
-                    Properties
-                </Link>
-                <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-xl flex items-center justify-center'>
-                    Services
-                </Link>
+        <>
+            <div className='flex items-center justify-between text-white navbar py-6 lg:py-3 px-5 md:px-8 lg:px-12 xl:px-28 2xl:px-36 3xl:px-40 sticky top-0 z-[9999]'>
+                <div className='flex items-center space-x-1'>
+                    <FaFire color='#703BF7' size={32} />
+                    <p>Estatein</p>
+                </div>
+                <div className='hidden lg:flex items-center gap-x-10'>
+                    <Link href="/" className={`px-7 py-4 border rounded-xl flex items-center border-transparent justify-center ${activeColor('/') ? 'bg-linkBackground border-linkBorder border-2' : 'hover:bg-linkBackground hover:border-linkBorder'}`}>
+                        Home
+                    </Link>
+                    <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-xl flex items-center justify-center'>
+                        About Us
+                    </Link>
+                    <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-xl flex items-center justify-center'>
+                        Properties
+                    </Link>
+                    <Link href="#" className='px-7 py-4 border-2 border-transparent hover:bg-linkBackground hover:border-linkBorder rounded-xl flex items-center justify-center'>
+                        Services
+                    </Link>
+                </div>
+
+                <div className='hidden lg:flex items-center justify-center bg-buttonBackground py-4 px-6 rounded-md border-linkBorder border'>
+                    <Link href="#">Contact Us</Link>
+                </div>
+                <div className="relative lg:hidden w-6 h-6 overflow-hidden" onClick={toggleMenu}>
+                    <div className={`absolute inset-0 transition-transform duration-300 ${isOpen ? 'rotate-180 scale-0' : 'rotate-0 scale-100'}`}>
+                        <HiMiniBars3BottomRight className="w-full h-full" />
+                    </div>
+                    <div className={`absolute inset-0 transition-transform duration-300 ${isOpen ? 'rotate-0 scale-100' : '-rotate-180 scale-0'}`}>
+                        <RiCloseFill className="w-full h-full" />
+                    </div>
+                </div>
+                <div className='absolute top-[5.5rem] right-5' ref={mobileRef}>
+                    <div className='relative'>
+                        {
+                            isOpen &&
+                            <div className='text-white navbar w-[200px] flex flex-col z-[9999] p-2 rounded-lg'>
+                                <Link className='hover:bg-buttonBackground p-3 rounded-md' href="/">Home</Link>
+                                <Link className='hover:bg-buttonBackground p-3 rounded-md' href="#">About Us</Link>
+                                <Link className='hover:bg-buttonBackground p-3 rounded-md' href="#">Properties</Link>
+                                <Link className='hover:bg-buttonBackground p-3 rounded-md' href="#">Services</Link>
+                                <Link className='bg-buttonBackground p-3 rounded-md text-center border border-linkBorder mt-2 font-semibold' href="#">Contact Us</Link>
+                            </div>
+                        }
+                    </div>
+                </div>
             </div>
 
-            <div className='hidden lg:flex items-center justify-center bg-buttonBackground py-4 px-6 rounded-md border-linkBorder border'>
-                <Link href="#">Contact Us</Link>
-            </div>
-            <div className="relative lg:hidden w-6 h-6 overflow-hidden" onClick={toggleMenu}>
-                <div className={`absolute inset-0 transition-transform duration-300 ${isOpen ? 'rotate-180 scale-0' : 'rotate-0 scale-100'}`}>
-                    <HiMiniBars3BottomRight className="w-full h-full" />
-                </div>
-                <div className={`absolute inset-0 transition-transform duration-300 ${isOpen ? 'rotate-0 scale-100' : '-rotate-180 scale-0'}`}>
-                    <RiCloseFill className="w-full h-full" />
-                </div>
-            </div>
-        </div>
+        </>
     )
 }
 
