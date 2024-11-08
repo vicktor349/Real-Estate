6dd70b35933fb78bed75b1cf3a30249f2b29f6cb
vicktor349 <ayomidevictor349@gmail.com>
2024-11-08 12:36:09 +0100

    start journey component

---
+++ components/StartJourney.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const StartJourney = () => {
+    return (
+        <div className='grid gap-y-10 sm:gap-y-20 my-10 mt-20 sm:mt-32'>
+            <div className='grid md:grid-cols-12 items-center gap-x-8'>
+                <div className='col-span-8'>
+                    <p className='text-2xl sm:text-4xl text-white font-semibold mb-2'>Start Your Real Estate Journey Today</p>
+                    <section className='text-[#999] leading-[27px]'>
+                        <p>Your dream property is just a click away. Whether you're looking for a new home, a strategic investment, or expert real estate advice, Estatein is</p>
+                        <p>here to assist you every step of the way. Take the first step towards your real estate goals and explore our available properties or get in touch</p>
+                        <p>with our team for personalized assistance.</p>
+                    </section>
+                </div>
+                <div className='col-span-4 ml-auto my-10 sm:mt-0'>
+                    <button className='bg-secondaryButtonBackground px-6 py-4 text-white rounded-lg whitespace-nowrap'>Explore Properties</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StartJourney
