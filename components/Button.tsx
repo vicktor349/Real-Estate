32c7666dc1a5052df54a3818ef3153e743aba935
vicktor349 <ayomidevictor349@gmail.com>
2024-11-19 10:18:39 +0100

    interface button props for button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { IconType } from 'react-icons'
 
 interface ButtonProps {
     text: string,
-    className?: string
+    className?: string,
+    icon?: IconType,
+    size?: number,
+    textClassName?: string,
+    iconClassName?: string,
 }
 
-const Button = ({ text, className }: ButtonProps) => {
+const Button = ({ text, className, icon: Icon, size, textClassName, iconClassName }: ButtonProps) => {
     return (
-        <button className={`bg-secondaryButtonBackground px-3 py-3 rounded-lg ${className}`}>
-            {text}
+        <button className={`bg-secondaryButtonBackground px-3 py-3 rounded-lg text-white flex items-center ${className}`}>
+            {Icon && <Icon size={size} className={`${iconClassName}`} />}
+            <p className={`text-sm lg:text-base ${textClassName}`}>{text}</p>
         </button>
     )
 }
