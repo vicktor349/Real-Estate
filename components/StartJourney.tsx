b0d81ef25526921b457fea0cbc5552bb55f3e4f5
vicktor349 <ayomidevictor349@gmail.com>
2024-11-22 10:36:03 +0100

    fix: Adjusted width of button and spacing between button and text

--- components/StartJourney.tsx
+++ components/StartJourney.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const StartJourney = () => {
     return (
         <div className='grid gap-y-10 sm:gap-y-20 my-10 mt-20 sm:mt-32'>
-            <div className='grid md:grid-cols-12 items-center gap-x-8'>
-                <div className='col-span-8'>
+            <div className='grid md:grid-cols-12 items-center gap-x-8 gap-y-10'>
+                <div className='md:col-span-8'>
                     <p className='text-2xl sm:text-4xl text-white font-semibold mb-2'>Start Your Real Estate Journey Today</p>
                     <section className='text-[#999] leading-[27px]'>
                         <p>Your dream property is just a click away. Whether you're looking for a new home, a strategic investment, or expert real estate advice, Estatein is</p>
@@ -12,8 +12,8 @@ const StartJourney = () => {
                         <p>with our team for personalized assistance.</p>
                     </section>
                 </div>
-                <div className='col-span-4 ml-auto my-10 sm:mt-0'>
-                    <button className='bg-secondaryButtonBackground px-6 py-4 text-white rounded-lg whitespace-nowrap'>Explore Properties</button>
+                <div className='w-full md:col-span-4 ml-auto sm:mt-0'>
+                    <button className='bg-secondaryButtonBackground px-6 py-4 text-white rounded-lg whitespace-nowrap w-full'>Explore Properties</button>
                 </div>
             </div>
         </div>
