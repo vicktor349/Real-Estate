2391067b14d74a12cb53a4935fc23d6d3ccefc2b
vicktor349 <ayomidevictor349@gmail.com>
2024-11-23 12:13:24 +0100

    fix:Adjusted padding for card on smaller devices

--- components/services/PropertyValueCard.tsx
+++ components/services/PropertyValueCard.tsx
@@ -10,7 +10,7 @@ interface PropertyValueCardProps {
 const PropertyValueCard = ({ src, text, content }: PropertyValueCardProps) => {
     return (
         <div className='border border-linkBorder rounded-xl'>
-            <div className='p-8 sm:p-4 md:p-7 3xl:p-12 space-y-5 md:space-y-[30px]'>
+            <div className='p-5 sm:p-4 md:p-7 3xl:p-12 space-y-5 md:space-y-[30px]'>
                 <section className='flex items-center space-x-5'>
                     <Image src={src} alt={`Image showing ${text}`} width={500} height={500} className='w-[50px] h-[50px] sm:w-[45px] sm:h-[45px] md:w-[50px] md:h-[50px] 2xl:w-[62px] 2xl:h-[62px]' />
                     <p className='text-lg md:text-xl 2xl:text-2xl font-semibold text-white'>{text}</p>
